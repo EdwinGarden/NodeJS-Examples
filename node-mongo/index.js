c36cc11a8563c005c279c3964b841924295e4f63
Edwin <egarden@fenestrapro.com>
2018-10-14 07:09:03 +0200

    Mongo node module. Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const dbOperations = require('./operations.js');
 const url = 'mongodb://localhost:27017/conFusion';
 const dbname = 'conFusion';
 
@@ -13,31 +14,61 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
 
-    collection.insertOne({"name": "Uthappizza", "description": "Node test description"},
-    (err, result) => {
-        // nested insersion check in the callback
-        assert.equal(err, null);
+    dbOperations.insertDocument(db, {name: "Vadonut", description:"Test description"}
+        , "dishes", (result) => {
+            console.log('Insert document:\n', result.ops);
 
-        console.log('New item was inserted:\n');
-        console.log(result.ops);
+            dbOperations.findDocuments(db, "dishes", (docs) => {
+                console.log('Found documents ', docs);
 
-        collection.find({}).toArray((err, docs) => {
-            // nested find in the insersion callback
-            assert.equal(err, null);
+                dbOperations.updateDocument(db, {name: "Vadonut"}, 
+                { description: "Updated test description"}, "dishes",
+                (result) => {
 
-            console.log('Found:\n');
-            console.log(docs);
+                    console.log('Updated document:\n ', result.result);
 
-            db.dropCollection('dishes', (err, result) => {
-                // nested drop db in the find callback, so depends on success of all callback parents
-                assert.equal(err, null);
+                    dbOperations.findDocuments(db, "dishes", (docs) => {
+                        console.log('Found documents:\n ', docs);
 
-                console.log('Closing the database');
-                client.close();
+                        db.dropCollection("dishes", (result) => {
+                            console.log('Dropped collection ', result);
+
+                            client.close();
+                        });
+                    });
+                });
             });
         });
 
-    });
+
+
+
+    // const collection = db.collection("dishes");
+
+    // collection.insertOne({"name": "Uthappizza", "description": "Node test description"},
+    // (err, result) => {
+    //     // nested insersion check in the callback
+    //     assert.equal(err, null);
+
+    //     console.log('New item was inserted:\n');
+    //     console.log(result.ops);
+
+    //     collection.find({}).toArray((err, docs) => {
+    //         // nested find in the insersion callback
+    //         assert.equal(err, null);
+
+    //         console.log('Found:\n');
+    //         console.log(docs);
+
+    //         db.dropCollection('dishes', (err, result) => {
+    //             // nested drop db in the find callback, so depends on success of all callback parents
+    //             assert.equal(err, null);
+
+    //             console.log('Closing the database');
+    //             client.close();
+    //         });
+    //     });
+
+    // });
 });
