45edd49cd47ad43c39e98c226abc10a3c8e6fb69
Edwin <egarden@fenestrapro.com>
2018-10-15 06:57:59 +0200

    Mongo promises example

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,70 +5,37 @@ const dbOperations = require('./operations.js');
 const url = 'mongodb://localhost:27017/conFusion';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-    
-    // check that the error is not null
-    assert.equal(err,null);
-
+MongoClient.connect(url).then((client) => {    
     // connection is good
     console.log('Connected to server');
 
     const db = client.db(dbname);
 
     dbOperations.insertDocument(db, {name: "Vadonut", description:"Test description"}
-        , "dishes", (result) => {
+        ,"dishes")
+        .then((result) => {
             console.log('Insert document:\n', result.ops);
+            return dbOperations.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log('Found documents: ');
+            return dbOperations.updateDocument(db, {name: "Vadonut"}, {description: "Updated test"}, "dishes");
+        })
+        .then((result) => {
+            console.log("Updated document:\n", result.result);
+            return dbOperations.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log('Found updated documents:\n', docs);
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log('Dropped collection: ', result);
+            return client.close();
+        })
+        .catch((err) => console.log(err));
 
-            dbOperations.findDocuments(db, "dishes", (docs) => {
-                console.log('Found documents ', docs);
-
-                dbOperations.updateDocument(db, {name: "Vadonut"}, 
-                { description: "Updated test description"}, "dishes",
-                (result) => {
-
-                    console.log('Updated document:\n ', result.result);
-
-                    dbOperations.findDocuments(db, "dishes", (docs) => {
-                        console.log('Found updated documents:\n ', docs);
-
-                        db.dropCollection("dishes", (result) => {
-                            console.log('Dropped collection ', result);
-
-                            client.close();
-                        });
-                    });
-                });
-            });
-        });
-
-
+    })
+    .catch((err) => console.log(err));
 
 
-    // const collection = db.collection("dishes");
-
-    // collection.insertOne({"name": "Uthappizza", "description": "Node test description"},
-    // (err, result) => {
-    //     // nested insersion check in the callback
-    //     assert.equal(err, null);
-
-    //     console.log('New item was inserted:\n');
-    //     console.log(result.ops);
-
-    //     collection.find({}).toArray((err, docs) => {
-    //         // nested find in the insersion callback
-    //         assert.equal(err, null);
-
-    //         console.log('Found:\n');
-    //         console.log(docs);
-
-    //         db.dropCollection('dishes', (err, result) => {
-    //             // nested drop db in the find callback, so depends on success of all callback parents
-    //             assert.equal(err, null);
-
-    //             console.log('Closing the database');
-    //             client.close();
-    //         });
-    //     });
-
-    // });
-});
