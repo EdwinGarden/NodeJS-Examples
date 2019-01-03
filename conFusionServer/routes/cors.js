dd7dafc94e563e080956df5ca7ae3020c8b1894a
Edwin <egarden@fenestrapro.com>
2019-01-03 07:34:54 +0100

    Added prelim CORS

--- conFusionServer/routes/cors.js
+++ conFusionServer/routes/cors.js
@@ -4,6 +4,9 @@ const app = express();
 
 const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 
+//** This doesn't seem to be a full implementation..
+//** Maybe check this out.. https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b */
+
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
